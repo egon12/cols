6025b52583bc6f71a146e3e011c7f02d3ef47f5a
Egon Firman <egon.firman@tokopedia.com>
2022-03-21 02:33:02 +0700

    need to see test

---
+++ examples/example1_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/egon12/cols"
+)
+
+type Person struct {
+	name string
+	age  int32
+}
+
+var persons = []Person{
+	{name: "gilbert", age: 32},
+	{name: "johnson", age: 16},
+	{name: "dwayne", age: 45},
+}
+
+func ExampleFilterMap() {
+	in := persons
+	temp := cols.Filter(in, func(p Person) bool { return p.age > 20 })
+	res := cols.Map(temp, func(p Person) string { return p.name })
+	fmt.Println(res)
+}
